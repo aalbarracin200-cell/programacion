8719e91645d90fd809d5c3d7173c588eaeb775a4
aalbarracin200-cell <aalbarracin200@unab.edu.co>
2026-03-18 11:54:17 -0500

    Add files via upload

---
+++ libro.py
@@ -0,0 +1,37 @@
+class Estudiante:
+    def __init__ (self, nombre, codigo, notas, promedio, ):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.notas = notas
+        
+    def calcular_definitiva(self):
+        return calcular_definitiva(self.notas)
+    
+    def 
+    def validar_nota(notas):
+        return 0<= nota <= 5
+        
+    def calcular_definitiva(notas):
+        return sum(notas) / len(notas)
+    
+    def calcular(definitiva):
+     if definitiva == 5:
+         return "exelente"
+    if definitiva >= 4.5:
+        return "alto"
+    if definitiva >= 3:
+        return "bajo"
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
